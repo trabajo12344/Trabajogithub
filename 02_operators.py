ff0b4e27a75241c7f8c903ba2802f2756196e3a7
trabajo12344 <davidgumera.w.m@gmail.com>
2025-06-03 23:05:38 -0300

    02_operators.py
    le modifique los datos de los prints y la forma de los calculos

--- 02_operators.py
+++ 02_operators.py
@@ -13,41 +13,41 @@ print(2 ** 3)
 print(2 ** 3 + 3 - 7 / 1 // 4)
 
 # Operaciones con cadenas de texto
-print("Hola " + "Python " + "¿Qué tal?")
-print("Hola " + str(5))
+print("tonto " + "tu " + "que por que?")
+print("nono " + str(5))
 
 # Operaciones mixtas
-print("Hola " * 5)
-print("Hola " * (2 ** 3))
+print("nono " * 5)
+print("nono " * (2 ** 3))
 
 my_float = 2.5 * 2
-print("Hola " * int(my_float))
+print("nono " * int(my_float))
 
 ### Operadores Comparativos ###
 
 # Operaciones con enteros
-print(3 > 4)
-print(3 < 4)
-print(3 >= 4)
+print(5 > 4)
+print(2 < 4)
+print(4 >= 4)
 print(4 <= 4)
-print(3 == 4)
+print(4 == 4)
 print(3 != 4)
 
 # Operaciones con cadenas de texto
-print("Hola" > "Python")
-print("Hola" < "Python")
-print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
-print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
-print("Hola" <= "Python")
-print("Hola" == "Hola")
-print("Hola" != "Python")
+print("sisi" > "nono")
+print("sisi" < "nono")
+print("aaaa" >= "absaa")  # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("absaa"))  # Cuenta caracteres
+print("sisi" <= "nono")
+print("sisi" == "nono")
+print("sisi" != "nono")
 
 ### Operadores Lógicos ###
 
 # Basada en el Álgebra de Boole https://es.wikipedia.org/wiki/%C3%81lgebra_de_Boole
-print(3 > 4 and "Hola" > "Python")
-print(3 > 4 or "Hola" > "Python")
-print(3 < 4 and "Hola" < "Python")
-print(3 < 4 or "Hola" > "Python")
-print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(3 > 4 and "sisi" > "nono")
+print(3 > 4 or "sisi" > "nono")
+print(3 < 4 and "sisi" < "nono")
+print(3 < 4 or "sisi" > "nono")
+print(3 < 4 or ("sisi" > "nono" and 4 == 4))
 print(not (3 > 4))
