daa57cdd1184bcff11cc4ab908d377aaa89ec9f6
trabajo12344 <davidgumera.w.m@gmail.com>
2025-07-02 06:45:36 -0300

    codigodepersonaje.py

--- codigodepersonaje.py
+++ codigodepersonaje.py
@@ -1,8 +1,8 @@
 import  random 
-nombres = ["david","ciro", "lauti"];
-razas = ["humano", "enano","elfo"];
-clases = ["guerrero", "Mago", "Arquero"];
-num=random.randint(0,2);
+nombres = ["David","Ciro", "Lauti","Tiziano","Emiliano","Axel","Alexis","Hernan","Sofia","Caro","Mailen","Vanesa","Pepe", "Francisco", "Lopez"];
+razas = ["Humano", "Enano","Elfo", "Orco","Hobbit","Gnomo","Semielfo","Semiorco","Tiefling","Dragónide" ,"Hada","Goblin","Hombre lagarto", "Aasimar"];
+clases = ["Guerrero", "Mago", "Arquero","Clérigo","Chorro","Bárbaro","Bardo","Hechicero","Druida","Monje", "Paladín","ranger","Nigromante","Artificiero", " Cazador de monstruos"];
+num=random.randint(0,14);
 vida = random.randint(50,100);
 ataque = random.randint(10,50);
 defensa = random.randint(5,30);
